91f3d330c6c29377823f96483b74fff05cc96230
Leonard Schuetz <leni.schuetz@me.com>
2017-02-02 20:25:56 +0100

    Added STORER instruction

--- src/StackMachine/opcode.cr
+++ src/StackMachine/opcode.cr
@@ -23,34 +23,35 @@ module StackMachine
     LOADR = 0x12 # loads given value into given register
     LOAD = 0x13 # loads value at fp + diff in the stack
     STORE = 0x14 # stores value at location fp + diff
-    MOV = 0x15 # copies contents of src register into dst register
+    STORER = 0x15 # stores value in register at location fp + diff
+    MOV = 0x16 # copies contents of src register into dst register
 
-    PUSHR = 0x16 # pushes value in given register
-    PUSH = 0x17 # push a given value
-    POP = 0x18 # pops a value from the stack into given register
+    PUSHR = 0x17 # pushes value in given register
+    PUSH = 0x18 # push a given value
+    POP = 0x19 # pops a value from the stack into given register
 
-    CMP = 0x19 # pops top two values and pushes 0 if they are equal
-    LT = 0x1A # pops top two values and pushes 0 if lower < upper
-    GT = 0x1B # pops top two values and pushes 0 if lower > upper
+    CMP = 0x1A # pops top two values and pushes 0 if they are equal
+    LT = 0x1B # pops top two values and pushes 0 if lower < upper
+    GT = 0x1C # pops top two values and pushes 0 if lower > upper
 
-    JZ = 0x1C # jumps to absolute address if top of the stack is 0
-    RJZ = 0x1D # adds differential to ip if top of the stack is 0
-    JNZ = 0x1E # jumps to absolute address if top of the stack is not 0
-    RJNZ = 0x1F # adds differential to ip if top of the stack is not 0
-    JMP = 0x20 # jumps to absolute address unconditionally
-    RJMP = 0x21 # adds differential to ip unconditionally
-    CALL = 0x22 # pushes current ip and unconditionally jumps to given address
-    RET = 0x23 # put top of the stack into ax and jump to address at the (now) top of stack
+    JZ = 0x1D # jumps to absolute address if top of the stack is 0
+    RJZ = 0x1E # adds differential to ip if top of the stack is 0
+    JNZ = 0x1F # jumps to absolute address if top of the stack is not 0
+    RJNZ = 0x20 # adds differential to ip if top of the stack is not 0
+    JMP = 0x21 # jumps to absolute address unconditionally
+    RJMP = 0x22 # adds differential to ip unconditionally
+    CALL = 0x23 # pushes current ip and unconditionally jumps to given address
+    RET = 0x24 # put top of the stack into ax and jump to address at the (now) top of stack
 
-    PREG = 0x24 # prints the contents of given register
-    PTOP = 0x25 # prints top of stack (doesn't pop)
+    PREG = 0x25 # prints the contents of given register
+    PTOP = 0x26 # prints top of stack (doesn't pop)
 
-    HALT = 0x26 # sets ext to given code and halts the machine
-    NOP = 0x27 # does nothing
+    HALT = 0x27 # sets ext to given code and halts the machine
+    NOP = 0x28 # does nothing
 
     @[AlwaysInline]
     def self.valid(instruction : Int32)
-      instruction >= 0 && instruction <= 0x27
+      instruction >= 0 && instruction <= 0x28
     end
   end
 end
