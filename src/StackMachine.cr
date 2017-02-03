b8533fa496d3830b42d528e8fc8af54d86bcb3e4
Leonard Schuetz <leni.schuetz@me.com>
2017-02-03 23:28:55 +0100

    updated entry code

--- src/StackMachine.cr
+++ src/StackMachine.cr
@@ -7,11 +7,36 @@ module StackMachine
   vm = VM.new
 
   program = Program{
+
+    # this is the bytecode representation of the following
+    # pseudo-code
+    #
+    # print(1 + 2 + add(3, 4))
+    # func add(a, b) { return a + b }
+
+    # calculate the left part
     PUSH, 1,
-    JZ, 7,
-    PUSH, 25,
+    PUSH, 2,
+    ADD,
+
+    # push arguments
+    PUSH, 3,
+    PUSH, 4,
+    PUSH, 2,
+
+    # call the method
+    CALL, 19,
+    PUSHR, AX,
+    ADD,
     PTOP,
-    HALT, 0
+    HALT, 0,
+
+    # add function code
+    LOAD, -4,
+    LOAD, -3,
+    ADD,
+    POP, AX,
+    RET
   }
 
   vm.init(memory_size: 64) # 64 Int32 values
