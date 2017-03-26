6b6477d47ca01a42893f91323467ecdf820a4baf
Leonard Schuetz <leni.schuetz@me.com>
2017-03-26 21:21:00 +0200

    Added instruction length decoding method

--- spec/machine_spec.cr
+++ spec/machine_spec.cr
@@ -61,7 +61,39 @@ describe StackVM::Machine do
     instruction.flag_s.should eq true
     instruction.flag_t.should eq false
     instruction.flag_b.should eq true
-    instruction.opcode.should eq 0b00011100_u8
+    instruction.opcode.should eq StackVM::Semantic::OP::LOADI
+  end
+
+  it "decodes the length of the LOADI instruction" do
+    machine = StackVM::Machine::Machine.new 32
+    machine.flash Slice(UInt8).new(8).tap { |memory|
+
+      # LOADI
+      memory[0] = 0b00000000_u8
+      memory[1] = 0b00011100_u8
+
+      # WORD
+      memory[2] = 0b00000000_u8
+      memory[3] = 0b00000000_u8
+      memory[4] = 0b00000000_u8
+      memory[5] = 0b00000010_u8
+
+      # 25
+      memory[6] = 0b00000000_u8
+      memory[7] = 0b00011001_u8
+    }
+
+    instruction = machine.fetch
+
+    instruction.should be_a StackVM::Machine::Instruction
+    instruction.flag_s.should eq false
+    instruction.flag_t.should eq false
+    instruction.flag_b.should eq false
+    instruction.opcode.should eq StackVM::Semantic::OP::LOADI
+
+    length = machine.decode_instruction_length instruction
+
+    length.should eq 8
   end
 
 end
--- src/stackvm/machine/machine.cr
+++ src/stackvm/machine/machine.cr
@@ -81,6 +81,58 @@ module StackVM::Machine
       p1 = Pointer(UInt16).new bytes.to_unsafe.address
       Instruction.new p1[0]
     end
+
+    # Returns the amount of bytes the current instruction takes up
+    #
+    # This reads type arguments from memory and returns the length of
+    # variable-length instructions
+    #
+    # Example:
+    # `LOADI BYTE 25` would use 7 bytes
+    # `LOADI WORD 25` would use 8 bytes
+    # `LOADI DWORD 25` would use 10 bytes
+    # `LOADI QWORD 25` would use 14 bytes
+    def decode_instruction_length(instruction : Instruction)
+
+      # Check for fixed length instructions first
+      case instruction.opcode
+      when OP::ADD, OP::SUB, OP::MUL, OP::DIV, OP::REM,
+           OP::EXP, OP::CMP, OP::LT, OP::GT, OP::LTE, OP::GTE, OP::SHR,
+           OP::SHL, OP::AND, OP::XOR, OP::NAND, OP::OR, OP::NOT, OP::RET, OP::NOP, OP::HALT
+        return 2
+      when OP::RPUSH, OP::RPOP, OP::INCR, OP::DECR, OP::JZR, OP::JNZR, OP::JMPR, OP::CALLR
+        return 3
+      when OP::MOV
+        return 4
+      when OP::PUTS
+        return 6
+      when OP::LOADR, OP::STORER, OP::READR, OP::WRITER
+        return 7
+      when OP::COPY, OP::COPYR
+        return 8
+      when OP::TRUNC, OP::SE, OP::ZE, OP::JZ, OP::JNZ, OP::JMP, OP::CALL
+        return 10
+      when OP::LOAD, OP::STORE, OP::INC, OP::DEC, OP::READ, OP::WRITE
+        return 14
+      when OP::LOADI
+        address = @regs[Reg::IP] + 2
+        type : Slice(UInt8)
+
+        begin
+          type = @memory[address, 4]
+          type.reverse! # flip because of endianness
+        rescue e : IndexError
+          raise Error.new Err::ILLEGAL_MEMORY_ACCESS, "Could not fetch type argument at #{address.to_s(16)}"
+        end
+
+        p1 = Pointer(UInt32).new type.to_unsafe.address
+        value_bytes = p1[0]
+
+        return 2 + 4 + value_bytes # instruction + argument + value
+      else
+        raise Error.new Err::INVALID_INSTRUCTION, "#{instruction.opcode.to_s(16)} is not a valid instruction"
+      end
+    end
   end
 
 end
