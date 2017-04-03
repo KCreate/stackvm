341e5c170e408f98a4326630d4ef0efac845c8a2
Leonard Schuetz <leni.schuetz@me.com>
2017-04-03 20:19:04 +0200

    Semantic check instruction arguments

--- src/assembler/semantic.cr
+++ src/assembler/semantic.cr
@@ -24,6 +24,7 @@ module Assembler
       check_duplicate_constants
       check_constant_size
       check_duplicate_blocks
+      check_instruction_arguments
     end
 
     # Checks the module for duplicate constant definitions
@@ -87,6 +88,112 @@ module Assembler
           @warnings << "Duplicate block definition: #{name}"
         else
           visited << name
+        end
+      end
+    end
+
+    # Check the arguments of instructions
+    private def check_instruction_arguments
+      @mod.blocks.each do |block|
+        block.instructions.each do |instruction|
+          args = instruction.arguments
+
+          case instruction.mnemonic
+          when "rpush"
+            assert_arg instruction, [Register]
+          when "rpop"
+            assert_arg instruction, [Register, IntegerValue]
+          when "mov"
+            assert_arg instruction, [Register, Register]
+          when "loadi"
+            assert_arg instruction, [Register, IntegerValue, [IntegerValue, ByteArray]]
+          when "rst"
+            assert_arg instruction, [Register]
+          when "add", "sub", "mul", "div", "idiv", "rem", "irem",
+               "fadd", "fsub", "fmul", "fdiv", "frem", "fexp"
+            assert_arg instruction, [Register, Register, Register]
+          when "cmp", "lt", "gt", "ult", "ugt"
+            assert_arg instruction, [Register, Register, Register]
+          when "shr", "shl", "and", "xor", "nand", "or"
+            assert_arg instruction, [Register, Register, Register]
+          when "not"
+            assert_arg instruction, [Register, Register]
+          when "load", "loadr"
+            assert_arg instruction, [Register, IntegerValue, [IntegerValue, Label]]
+          when "pushs"
+            assert_arg instruction, [IntegerValue, IntegerValue]
+          when "loads"
+            assert_arg instruction, [IntegerValue, Register]
+          when "store"
+            assert_arg instruction, [IntegerValue, Register]
+          when "read"
+            assert_arg instruction, [Register, IntegerValue, IntegerValue]
+          when "readc"
+            assert_arg instruction, [Register, IntegerValue, IntegerValue]
+          when "reads"
+            assert_arg instruction, [Register, IntegerValue, IntegerValue]
+          when "readcs"
+            assert_arg instruction, [Register, IntegerValue, IntegerValue]
+          when "write"
+            assert_arg instruction, [Register, Register]
+          when "writec"
+            assert_arg instruction, [[IntegerValue, Label], Register]
+          when "writes"
+            assert_arg instruction, [Register, IntegerValue]
+          when "writecs"
+            assert_arg instruction, [[IntegerValue, Label], IntegerValue]
+          when "copy"
+            assert_arg instruction, [Register, IntegerValue, Register]
+          when "copyc"
+            assert_arg instruction, [[IntegerValue, Label], IntegerValue, [IntegerValue, Label]]
+          when "jz"
+            assert_arg instruction, [[IntegerValue, Label]]
+          when "jzr"
+            assert_arg instruction, [Register]
+          when "jmp"
+            assert_arg instruction, [[IntegerValue, Label]]
+          when "jmpr"
+            assert_arg instruction, [Register]
+          when "call"
+            assert_arg instruction, [[IntegerValue, Label]]
+          when "callr"
+            assert_arg instruction, [Register]
+          when "ret"
+            assert_arg instruction, [] of ASTNode.class
+          when "nop"
+            assert_arg instruction, [] of ASTNode.class
+          when "syscall"
+            assert_arg instruction, [] of ASTNode.class
+          else
+            @errors << "Bug: unknown instruction mnemonic: #{instruction.mnemonic}"
+          end
+        end
+      end
+    end
+
+    # Check an instruction for a specific amount of arguments and types
+    private def assert_arg(instruction, types)
+      args = instruction.arguments
+
+      # Check argument count
+      unless args.size == types.size
+        @errors << "#{instruction.mnemonic} expects #{types.size} arguments, got #{args.size}"
+        return false
+      end
+
+      # Check each argument type
+      args.each_with_index do |arg, index|
+        type = types[index]
+        next if type == Argument
+
+        if type.is_a?(Array(ASTNode.class)) || type.is_a?(Array(Value.class))
+          unless type.includes? arg.class
+            @errors << "#{instruction.mnemonic}: expected arg #{index + 1} to be one of #{type}"
+          end
+        else
+          unless arg.class == types[index]
+            @errors << "#{instruction.mnemonic}: expected arg #{index + 1} to be a #{type}"
+          end
         end
       end
     end
