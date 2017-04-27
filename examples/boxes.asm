b2556c7cdd641db01d642bb55c90f30a577f86e2
Leonard Schuetz <leni.schuetz@me.com>
2017-04-27 18:44:42 +0200

    Added some graphics example code

---
+++ examples/boxes.asm
@@ -0,0 +1,77 @@
+.def counter_x r0
+.def counter_y r1
+.def comp r2
+.def s_zero r3
+.def s_one r4
+
+.label setup
+.label entry_addr
+  loadi counter_x,  vram_width
+  loadi counter_y,  vram_height
+  rst s_zero
+  loadi s_one,      1
+
+  sub counter_x, counter_x, s_one
+  sub counter_y, counter_y, s_one
+
+  push dword, msg_welcome
+  push dword, 29
+  push word, sys_write
+  syscall
+
+  push dword, msg_sleep
+  push dword, 26
+  push word, sys_write
+  syscall
+
+  push float, 3.0
+  push word, sys_sleep
+  syscall
+
+  jmp main
+
+.label main
+
+  rpush counter_x
+  rpush counter_y
+  push byte, 0b11100011
+  push dword, 9
+  call gfx_draw_pixel
+
+  jmp check_zero_reached1
+
+.label check_zero_reached1
+  cmp counter_x, s_zero
+  jz check_zero_reached2
+  jmp loop
+.label check_zero_reached2
+  cmp counter_y, s_zero
+  jz quit_program
+  jmp loop
+
+.label loop
+
+  cmp counter_x, s_zero
+  jz br1
+
+  sub counter_x, counter_x, s_one
+
+  jmp br2
+.label br1
+  sub counter_y, counter_y, s_one
+  loadi counter_x, vram_width
+  sub counter_x, counter_x, s_one
+
+.label br2
+
+  jmp main
+
+.label quit_program
+  push byte, 0
+  push word, sys_exit
+  syscall
+
+.db msg_welcome 29 "welcome to the graphics demo\n"
+.db msg_sleep 26 "sleeping for 3 seconds...\n"
+
+.include "graphics.asm"
---
+++ examples/graphics.asm
@@ -0,0 +1,35 @@
+; Constant definitions
+.def GFX_TMP                  r58
+.def GFX_TMP2                 r59
+.def GFX_TMP_BYTE             r58b
+
+; void gfx_draw_pixel(int x, int y, byte color)
+; x     : 17
+; y     : 13
+; color : 12
+.label gfx_draw_pixel
+  .def gfx_draw_pixel_offset      r0
+  .def gfx_draw_pixel_arg_x       17
+  .def gfx_draw_pixel_arg_y       13
+  .def gfx_draw_pixel_arg_color   12
+
+  ; backup register
+  rpush gfx_draw_pixel_offset
+
+  ; calculate offset into vram
+  loadi gfx_draw_pixel_offset, VRAM_ADDRESS
+  load GFX_TMP, gfx_draw_pixel_arg_x
+  add gfx_draw_pixel_offset, gfx_draw_pixel_offset, GFX_TMP
+  load GFX_TMP, gfx_draw_pixel_arg_y
+  loadi GFX_TMP2, VRAM_WIDTH
+  mul GFX_TMP, GFX_TMP, GFX_TMP2
+  add gfx_draw_pixel_offset, gfx_draw_pixel_offset, GFX_TMP
+
+  ; write the color
+  load GFX_TMP_BYTE, gfx_draw_pixel_arg_color
+  write gfx_draw_pixel_offset, GFX_TMP_BYTE
+
+  ; restore registers
+  rpop gfx_draw_pixel_offset
+
+  ret
